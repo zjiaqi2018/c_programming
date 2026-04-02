ab5595afce93160476fdfe4166bd2be9cac702f8
Jiaqi Zhang <jiaqizhang23@outlook.com>
2026-04-02 14:22:54 +0800

    lab06

---
+++ lab06/lab06.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Task 20: Input Validation and Parity Sums
+void task20() {
+    printf("--- Task 20 Output ---\n");
+
+}
+
+// Task 21: Nested While Loops
+void task21() {
+    printf("--- Task 21 Output ---\n");
+
+}
+
+// Task 22: Sentinel Loop
+void task22() {
+    printf("--- Task 22 Output ---\n");
+ 
+}
+
+// Task 23: Factorial
+void task23() {
+    printf("--- Task 23 Output ---\n");
+
+}
+
+// Task 24: Reverse Number
+void task24() {
+    printf("--- Task 24 Output ---\n");
+
+}
+
+int main() {
+    task20();
+    task21();
+    task22();
+    task23();
+    task24();
+    return 0;
+}
