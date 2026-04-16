e9fdc8b91d397a116f141e7bfeafd9035bd6b032
Jiaqi Zhang <jiaqizhang23@outlook.com>
2026-04-16 16:33:11 +0800

    lab07

---
+++ lab07/lab07.cc
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// Task 25: Multiples of 4 not 5
+void task25()
+{
+}
+
+// Task 26: BMI with conversion
+void task26()
+{
+}
+
+// Task 27: Custom Logic
+void task27()
+{
+}
+
+// Task 29: Triangle and Perimeter
+void task29()
+{
+}
+
+// Task 30: Switch with Pass
+void task30()
+{
+}
+
+int main()
+{
+    // Implement calls as needed
+    return 0;
+}
