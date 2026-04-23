b49bfb7efa9756834ee172a31c538b2d7a6d531f
Jiaqi Zhang <jiaqizhang23@outlook.com>
2026-04-23 16:12:54 +0800

    lab08

---
+++ lab08/lab08.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+// Task 31: Array Search
+void task31()
+{
+    printf("--- Task 31 Output ---\n");
+}
+
+// Task 32: ASCII to String
+void task32()
+{
+    printf("--- Task 32 Output ---\n");
+}
+
+// Task 33: Two-Way Sorting
+void task33()
+{
+    printf("--- Task 33 Output ---\n");
+}
+
+// Task 34: Math Computations
+void task34()
+{
+    printf("--- Task 34 Output ---\n");
+}
+
+// Task 35 Logic
+int differenceASCIICode(char c1, char c2)
+{
+}
+void task35()
+{
+    printf("--- Task 35 Output ---\n");
+}
+
+// Task 36 Logic
+float findMax(float a[], int n)
+{
+}
+void task36()
+{
+    printf("--- Task 36 Output ---\n");
+}
+
+// Task 37 Logic
+bool isDigitLetter(char c)
+{
+}
+void task37()
+{
+    printf("--- Task 37 Output ---\n");
+}
+
+int main()
+{
+    task31();
+    task32();
+    task33();
+    task34();
+    task35();
+    task36();
+    task37();
+    return 0;
+}
